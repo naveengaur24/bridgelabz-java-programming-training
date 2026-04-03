92745e35abe81cf2fe2063431d4329bc10c94b2e
Naveen Gaur <naveengaur343@gmail.com>
2026-04-03 12:26:02 +0530

    [Naveen Gaur] Add. Find factors using while

---
+++ Core Programming Intermediate/Control Flow/Level2/FactorsWhile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int i = 1;
+        while (i < num) {
+            if (num % i == 0)
+                System.out.print(i + " ");
+            i++;
+        }
+    }
+}
