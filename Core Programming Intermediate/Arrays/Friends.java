2e63ed80ae382cd5b79417f8e4f7655030953d86
Naveen Gaur <naveengaur343@gmail.com>
2026-04-04 20:57:06 +0530

    [Naveen Gaur] Added - Find Friends

---
+++ Core Programming Intermediate/Arrays/Friends.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Friends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] age = new int[3];
+        double[] height = new double[3];
+        String[] names = {"Amar", "Akbar", "Anthony"};
+
+        for (int i = 0; i < 3; i++) {
+            age[i] = sc.nextInt();
+            height[i] = sc.nextDouble();
+        }
+
+        int minAge = age[0], minIndex = 0;
+        double maxHeight = height[0];
+        int maxIndex = 0;
+
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < minAge) {
+                minAge = age[i];
+                minIndex = i;
+            }
+            if (height[i] > maxHeight) {
+                maxHeight = height[i];
+                maxIndex = i;
+            }
+        }
+
+        System.out.println("Youngest: " + names[minIndex]);
+        System.out.println("Tallest: " + names[maxIndex]);
+    }
+}
