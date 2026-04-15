1f88ee52df6effd4fafeb202535a6d7ef0a7dd50
Naveen Gaur <naveengaur343@gmail.com>
2026-04-15 11:25:22 +0530

    Remove Core Programming Basics

--- Core Programming Basics/Level1/Discount_Calculation.java
+++
@@ -1,9 +0,0 @@
-public class Discount_Calculation {
-    public static void main(String[] args) {
-        int fee=125000;
-        int dicountpercent=10;
-        int discountamount=(fee*dicountpercent)/100;
-        int discountedPrice=fee-discountamount;
-        System.out.println("The discount amount is INR " + discountamount +" and final discounted fee is INR " + discountedPrice);
-    }
-}
--- Core Programming Basics/Level1/Pens_Distribution.java
+++
@@ -1,9 +0,0 @@
-public class Pens_Distribution {
-    public static void main(String[] args) {
-        int pens=14;
-        int students=3;
-        int pensPerStudent=pens/students;
-        int remainingPens=pens%students;
-        System.out.println("Each student gets " + pensPerStudent + " pens and " + remainingPens + " pens are left undistributed");
-    }
-}
