c7af04278f6567eeb0735449546e26d74806e753
Naveen Gaur <naveengaur343@gmail.com>
2026-05-01 23:17:52 +0530

    Added Desing code

---
+++ Design_Modeling/DesingPatterns/Singleton.java
@@ -0,0 +1,7 @@
+package Design_Modeling.DesingPatterns;
+
+public class Singleton {
+    public static void main(String[] args) {
+        System.out.println("Singleton Design Pattern Example");
+    }
+}
