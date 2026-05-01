a51c99ae1b51c42bf2ed8ec2c9fc7e54765fa5fa
Naveen Gaur <naveengaur343@gmail.com>
2026-05-01 22:54:19 +0530

    [Naveen Gaur] Added. SupressWarnings Annotation

---
+++ AdvanceJava/Annotations/SuppressWarningsDemo.java
@@ -0,0 +1,15 @@
+package AdvanceJava.Annotations;
+import java.util.*;
+
+public class SuppressWarningsDemo {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList();
+
+        list.add("Java");
+        list.add(10);
+
+        System.out.println(list);
+    }
+}
